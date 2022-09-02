eaea2cc5fd373403e1a2bcf6829da8c00e6b55f1
George Fortetsanakis <gfortetsanakis@gmail.com>
2022-09-02 10:43:04 +0300

    Reformatted code

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-locals {
-  jupyter_notebook_image_name   = split(":", var.jupyter_notebook_image)[0]
-  jupyter_notebook_image_tag    = split(":", var.jupyter_notebook_image)[1]
-  create_docker_registry_secret = (var.docker_registry_username != "") && (var.docker_registry_password != "") ? true : false
-  docker_registry_secret_name   = local.create_docker_registry_secret ? "spark-docker-registry-secret" : ""
-}
-
 resource "kubernetes_secret" "docker_registry_secret" {
   count = local.create_docker_registry_secret ? 1 : 0
 
